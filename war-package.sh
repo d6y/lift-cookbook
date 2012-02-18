1eaf9c4e8d29b5001827b9ca38b333dcc6dcf330
Richard Dallaway <richard@spiralarm.com>
2012-02-18 20:14:40 +0000

    Copies docs/img to target/docs/img/

--- war-package.sh
+++ war-package.sh
@@ -1,5 +1,6 @@
 # Packages Pamflet output into just good enough WAR for deployment
 # For use after: sbf write-pamflet
+cp docs/img/* target/docs/img/ 
 echo "<jsp:forward page='Lift+Cookbook.html'/>" > target/docs/index.jsp
 echo "" >> target/docs/pamflet.manifest
 echo "#`date`" >> target/docs/pamflet.manifest
