649237cfd4153a6ebe62656749e1a2d752454496
Richard Dallaway <richard@spiralarm.com>
2012-02-18 17:14:24 +0000

    Added a comment to the manifest in an attempt to defeat caching

--- war-package.sh
+++ war-package.sh
@@ -1,6 +1,7 @@
 # Packages Pamflet output into just good enough WAR for deployment
 # For use after: sbf write-pamflet
 echo "<jsp:forward page='Lift+Cookbook.html'/>" > target/docs/index.jsp
+echo "" >> target/docs/pamflet.manifest
 echo "#`date`" >> target/docs/pamflet.manifest
 rm -f target/docs.war
 jar cf target/docs.war -C target/docs .
