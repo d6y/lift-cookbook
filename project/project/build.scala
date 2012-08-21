0ee5e37ce2cac39befde0bf28d3046e4591dca1d
Richard Dallaway <richard@spiralarm.com>
2012-08-21 11:48:35 +0100

    Pamflet 0.4.0

--- project/project/build.scala
+++ project/project/build.scala
@@ -1,6 +1,5 @@
 import sbt._
 object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn( 
-    pamflet)
-  lazy val pamflet = uri("git://github.com/n8han/pamflet-plugin#0.3.0")
+  lazy val root = Project("plugins", file(".")) dependsOn(pamflet)
+  lazy val pamflet = uri("git://github.com/n8han/pamflet-plugin#0.4.0")
 }
